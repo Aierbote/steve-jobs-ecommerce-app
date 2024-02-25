a0b9c7eaa498d69b1b73e9e833e98fda8341b852
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-25 18:45:08 +0100

    feat: preliminary ProductsList with style

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,6 +2,28 @@ import { AppContext } from "@/ContextProvider";
 import { Product } from "@/declarations";
 import { useContext } from "react";
 
+const cardStyle = {
+	width: "23rem",
+	height: "26rem",
+	border: "0.0625rem solid #00000029",
+	borderRadius: "1rem",
+
+	// &:hover {
+	//   boxShadow: "0 0 10px 0 #00000029",
+	// }
+};
+
+const gridStyle = {
+	width: "90vw",
+	padding: "0",
+	margin: "1rem auto",
+	overflow: "hidden",
+	display: "grid",
+	gridTemplateColumns: "repeat(3, minmax(20rem, 0.8fr))",
+	gridColumnGap: "0.5rem",
+	rowGap: "1rem",
+};
+
 export function ProductCard({
 	qty,
 	userId,
@@ -15,7 +37,7 @@ export function ProductCard({
 	const { addToCart } = useContext(AppContext);
 
 	return (
-		<div key={id}>
+		<div key={id} style={cardStyle}>
 			<img src={thumbnail} alt={title} />
 			<h3>{title}</h3>
 			<p>{description}</p>
@@ -33,6 +55,29 @@ export function ProductCard({
 			>
 				Add To Cart
 			</button>
+		</div>
+	);
+}
+
+export function ProductsList({ products }: { products: Product[] | null }) {
+	return (
+		<div style={gridStyle}>
+			{!products && <p>Loading Products...</p>}
+			{products?.map(
+				({ qty, userId, title, description, id, price, image, thumbnail }) => (
+					<ProductCard
+						key={id}
+						qty={qty}
+						userId={userId}
+						title={title}
+						description={description}
+						id={id}
+						price={price}
+						image={image}
+						thumbnail={thumbnail}
+					/>
+				)
+			)}
 		</div>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import Head from "next/head";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import { use, useContext, useEffect } from "react";
 import { AppContext } from "@/ContextProvider";
-import { Product, TContext } from "@/declarations";
-import { ProductCard } from "@/components/card";
+import { ProductsList } from "@/components/card";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,22 +13,7 @@ export default function Home() {
 		<>
 			<h1>Home</h1>
 			<h2>Our Products</h2>
-			{!products && <p>Loading Products...</p>}
-			{products?.map(
-				({ qty, userId, title, description, id, price, image, thumbnail }) => (
-					<ProductCard
-						key={id}
-						qty={qty}
-						userId={userId}
-						title={title}
-						description={description}
-						id={id}
-						price={price}
-						image={image}
-						thumbnail={thumbnail}
-					/>
-				)
-			)}
+			<ProductsList products={products} />
 		</>
 	);
 }
