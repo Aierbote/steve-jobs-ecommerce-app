e61c01ffff93b7ce2949631e81dc341b3553c56c
Alberto Cangialosi <billyballo.res@gmail.com>
2024-02-27 15:51:47 +0100

    feat: follow Static Exports from documentation
    NEXT JS DOCUMENTATION:
    https://nextjs.org/docs/pages/building-your-application/deploying/static-exports

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/steve-jobs-ecommerce-app",
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: "/steve-jobs-ecommerce-app",
+//   output: "export",  // <=== enables static exports
+//   reactStrictMode: true,
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+/**
+ * @type {import('next').NextConfig}
+*/
+const nextConfig = {
+  output: 'export',
+  basePath: "/steve-jobs-ecommerce-app",
+  reactStrictMode: true,
+
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
