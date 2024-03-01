a56c07bfc50fa017086df890be285f89def4621d
Alberto Cangialosi <billyballo.res@gmail.com>
2024-03-01 01:16:48 +0100

    feat: redirecting in of `!paid`

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,23 @@
+import { AppContext } from "@/ContextProvider";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+
 export default function Success() {
-  return <h1>Success</h1>;
+	const { paid } = useContext(AppContext);
+	const router = useRouter();
+
+	if (!paid) {
+		setTimeout(() => {
+			router.push("/");
+		}, 5000);
+
+		return (
+			<>
+				<h1>Not paid yet</h1>
+				<h3>Redirecting you to Homepage</h3>
+			</>
+		);
+	}
+
+	return <h1>Success</h1>;
 }
